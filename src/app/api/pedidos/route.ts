caf0d71f884deceff294c53e3a4e2f31a89af667
raphauy <rapha.uy@rapha.uy>
2023-11-20 09:52:13 -0300

    First commit

--- src/app/api/pedidos/route.ts
+++ src/app/api/pedidos/route.ts
@@ -17,25 +17,11 @@ export async function POST(request: Request, { params }: Props ) {
         if (!apiToken) return NextResponse.json({ error: "apiToken is required" }, { status: 400 })
         if (apiToken !== process.env.API_TOKEN) return NextResponse.json({ error: "Bad apiToken" }, { status: 400 })
         
-        const inmobiliariaId = params.inmobiliariaId
-        if (!inmobiliariaId) return NextResponse.json({ error: "inmobiliariaId is required" }, { status: 400 })
-
         const json= await request.json()
         console.log("json: ", json)
 
-        const idPropiedad= json.idPropiedad
-        if (!idPropiedad) {
-            console.log("idPropiedad is required")            
-            return NextResponse.json({ error: "idPropiedad is required" }, { status: 400 })
-        }
+        return NextResponse.json({ error: "error: " + "Servicio no habilitado."}, { status: 502 })        
 
-        const updated= await createOrUpdatePropertyWithPrisma(json, inmobiliariaId)
-
-        if (!updated) return NextResponse.json({ error: "error updating property" }, { status: 502 })
-
-        await updateEmbedding(updated.id)
-
-        console.log("updated!")
         return NextResponse.json({ data: "ACK" }, { status: 200 })
 
     } catch (error) {
