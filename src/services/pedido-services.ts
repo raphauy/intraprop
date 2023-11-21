11dbdc7138eddd44e448daf1a73d2ee18e248744
raphauy <rapha.uy@rapha.uy>
2023-11-21 14:39:29 -0300

    small change

--- src/services/pedido-services.ts
+++ src/services/pedido-services.ts
@@ -194,7 +194,8 @@ export async function runThread(pedidoId: string) {
     createdThread.id, 
     { 
       assistant_id: OPENAI_ASSISTANT_ID,
-      model: "gpt-3.5-turbo-1106",
+//      model: "gpt-3.5-turbo-1106",
+      model: "gpt-4-1106-preview",
     }
   )
 
