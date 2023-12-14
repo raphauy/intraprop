3dc83d22590d8b51443681c83eea0f44a5ad3430
raphauy <rapha.uy@rapha.uy>
2023-12-14 09:12:06 -0300

    feature: dormitorios a variable fija

--- src/app/admin/side-bar.tsx
+++ src/app/admin/side-bar.tsx
@@ -103,7 +103,7 @@ export default function SideBar() {
       <section className="mb-4">
         {divider()}
         
-        <p className="hidden sm:block lg:w-36 ml-3 font-bold">Versión 0.12.13</p>                  
+        <p className="hidden sm:block lg:w-36 ml-3 font-bold">Versión 0.12.14</p>                  
       </section>
     </div>
   );
