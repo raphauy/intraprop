44048d5e130ab78375debe299b503c35e030aa63
raphauy <rapha.uy@rapha.uy>
2024-01-13 16:33:18 -0300

    tooltip

--- src/app/admin/tablero/pedido-box.tsx
+++ src/app/admin/tablero/pedido-box.tsx
@@ -1,5 +1,6 @@
 import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card"
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
 import { formatPresupuesto } from "@/lib/utils"
 import { PedidoDAO } from "@/services/pedido-services"
@@ -20,27 +21,28 @@ export default function PedidoBox({pedido, cantCoincidencias}: Props) {
         <Card>
             <CardHeader className="px-4">
                 <div className="flex justify-between">
-                    <div className="flex gap-10">
+                    <div className="flex gap-10 items-start">
                         <CardTitle>Pedido {numberFormatted}</CardTitle>
                         <TooltipProvider delayDuration={0}>
-                            <Tooltip>
-                                <TooltipTrigger asChild>
-                                    <MessageCircle  size={20} />
-                                </TooltipTrigger>
-                                <TooltipContent className="w-72">
+                            <Popover>
+                                <PopoverTrigger>
+                                    <Tooltip>
+                                        <TooltipTrigger asChild>
+                                            <MessageCircle  size={20} />
+                                        </TooltipTrigger>
+                                        <TooltipContent className="w-72 text-left">
+                                            <p className="mb-5 text-lg font-bold">Texto original del pedido:</p>
+                                            <p className="text-sm whitespace-pre-wrap">{pedido.text}</p>
+                                        </TooltipContent>
+                                    </Tooltip>
+
+                                </PopoverTrigger>
+                                <PopoverContent className="w-72 text-left">
                                     <p className="mb-5 text-lg font-bold">Texto original del pedido:</p>
                                     <p className="text-sm whitespace-pre-wrap">{pedido.text}</p>
-                                </TooltipContent>
-                            </Tooltip>
+                                </PopoverContent>
+                            </Popover>
                         </TooltipProvider>
-
-                        <HoverCard>
-                            <HoverCardTrigger><Info size={20}/></HoverCardTrigger>
-                            <HoverCardContent className="w-72">
-                                <p className="mb-5 text-lg font-bold">Texto original del pedido:</p>
-                                <p className="text-sm whitespace-pre-wrap">{pedido.text}</p>
-                            </HoverCardContent>
-                        </HoverCard>
                     </div>
                     <p className="text-green-400 font-bold flex items-center gap-1 text-2xl"><Target size={20} />{cantCoincidencias}</p>
                 </div>
